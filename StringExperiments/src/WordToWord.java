2625066b126639b1dd3e69041538c601bae751da
komat0 <111368527+komat0@users.noreply.github.com>
2023-04-04 17:31:33 +0700

    Done: Возьмите русский текст (не менее 100 слов) и напишите программу, которая будет разбивать его на слова и печатать слова в консоли. Знаки препинания не являются частью слова.

--- StringExperiments/src/WordToWord.java
+++ StringExperiments/src/WordToWord.java
@@ -4,10 +4,10 @@ public class WordToWord {
 
         String gettingText = "Дорогие, блин, гости. Здравствуйте! Однако, не каждый " +
                 "знает: где сидит физ-ра, а где культура и почему " +
-                "пуля   - дура, а штык - нет; 222    --  ООО 'Ромашка' " +
+                "пуля   - дура, а штык - нет; 222    -  ООО 'Ромашка' " +
                 "Nemnogo ^ teksta : NA ANGLIYSKOM";
         String space = " ";
-        String textOnly = gettingText.replaceAll("[^\\p{L}\\p{N}-\\s]|\\s+-\\s+", "")
+        String textOnly = gettingText.replaceAll("[^\\p{L}\\p{N}-\\s]|\\s+-+", "")
                 .replaceAll("\\s+", " ").trim();
         for (int i = 0; i < textOnly.length(); i++) {
             String a = String.valueOf(textOnly.charAt(i));
